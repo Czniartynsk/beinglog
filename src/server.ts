2b7e6ac09c547afd1ff1eba4f3a7a68e2150467d
Czniartynsk <bruno.as72@gmail.com>
2025-07-05 20:57:32 -0300

    Liberando cors para localhost

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import { app } from "@/app";
 import { env } from "./env";
+import cors from "cors";
 
 const PORT = env.PORT
+
+app.use(cors({
+  origin: "http://localhost:5173", // ou "*" durante testes
+  credentials: true
+}));
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
