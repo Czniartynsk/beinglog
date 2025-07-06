4097d7c00ddf50343b538cf3cd99f53dfdd149b6
Czniartynsk <bruno.as72@gmail.com>
2025-07-05 21:32:57 -0300

    ajuste no cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import "express-async-errors"
 
 import { errorHandling } from "./middlewares/error-handling"
 import { routes } from "./routes"
+import cors from "cors";
 
 const app = express()
+
+
+app.use(cors({
+  origin: "http://localhost:5173", // ou "*" durante testes
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json())
 app.use(routes)
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
 import { app } from "@/app";
 import { env } from "./env";
-import cors from "cors";
 
 const PORT = env.PORT
-
-app.use(cors({
-  origin: "http://localhost:5173", // ou "*" durante testes
-  credentials: true
-}));
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
